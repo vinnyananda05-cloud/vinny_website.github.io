8eeeb5c1fbee9de913a2b0d688c0aafaab7ad179
Vinny Ananda <vinnyananda05@gmail.com>
2026-04-01 17:21:07 +0700

    UTS Web Programming

---
+++ dist/js/script.js
@@ -0,0 +1,13 @@
+// Scrool Top Up
+
+const scrollUp = document.querySelector(".scroll-up");
+
+window.onscroll = () => {
+    if (window.scrollY > 500) {
+        scrollUp.classList.add("scroll-active");
+    } else {
+        scrollUp.classList.remove("scroll-active");
+    }
+};
+
+// Scrool Top Up
